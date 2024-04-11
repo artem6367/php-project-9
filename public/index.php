59b1e30c48a14c10844843baed397898286e9099
artem6367 <artem_ivan1998@mail.ru>
2024-04-11 10:29:56 +0700

    Подготовка проекта

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<?php
+
+use Psr\Http\Message\RequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+    $name = $args['name'];
+    $response->getBody()->write("Hello, $name");
+    return $response;
+});
+
+$app->run();
