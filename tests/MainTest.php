4fa8aefd5bb1423948c3db8994f38fc230e81867
artem6367 <artem_ivan1998@mail.ru>
2024-04-11 10:47:04 +0700

    Поддержка php версии 7.3. Нужно для Render.com

---
+++ tests/MainTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace tests;
+
+use PHPUnit\Framework\TestCase;
+
+class MainTest extends TestCase
+{
+    public function testMain(): void
+    {
+        $this->assertEquals(1, 1);
+    }
+}
